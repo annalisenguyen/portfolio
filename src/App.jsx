ccff909b9ad43914916adba4dd78c11da71ae1d8
Annalise Nguyen <annalise.nguyen05@gmail.com>
2026-01-08 18:37:19 -0500

    Remove admin functionality and authentication context. Deleted related components, routes, and styles to streamline the application for a public-facing experience.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import BlogPost from './pages/BlogPost'
-import Login from './pages/Login'
-import AdminDashboard from './pages/AdminDashboard'
-import PostForm from './pages/PostForm'
-import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
-      {/* Public routes */}
       <Route path="/" element={
         <Layout>
           <Home />
@@ -20,30 +15,6 @@ function App() {
         <Layout>
           <BlogPost />
         </Layout>
-      } />
-      
-      {/* Admin routes */}
-      <Route path="/admin/login" element={<Login />} />
-      <Route path="/admin" element={
-        <ProtectedRoute>
-          <Layout>
-            <AdminDashboard />
-          </Layout>
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/add" element={
-        <ProtectedRoute>
-          <Layout>
-            <PostForm />
-          </Layout>
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/edit/:slug" element={
-        <ProtectedRoute>
-          <Layout>
-            <PostForm />
-          </Layout>
-        </ProtectedRoute>
       } />
     </Routes>
   )
